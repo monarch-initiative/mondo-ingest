5e5cffac82203880bc04f746b8d7dfd798488e25
Harshad Hegde <hegdehb@gmail.com>
2022-07-19 09:39:34 -0500

    basic boilerplate code: does nothing

--- src/scripts/match-mondo-sources-all-lexical.py
+++ src/scripts/match-mondo-sources-all-lexical.py
@@ -10,3 +10,30 @@
 # Use synonymiser
 # Use oak.mapping() pipeline
 
+import logging
+from oaklib.cli import lexmatch
+import sys
+
+def run(input:str, lexical_index: str, output: str):
+    # args = [input,lexical_index,output]
+    # msdf = lexmatch(*args)
+    import pdb; pdb.set_trace()
+
+
+
+if __name__ == '__main__':
+    input = None
+    lexical_index = None
+    output = None
+
+    for arg in sys.argv:
+        if arg.endswith('.owl'):
+            input = arg
+        elif arg.endswith('.yaml') or arg.endswith('.yml'):
+            lexical_index = arg
+        elif arg.endswith('.tsv'):
+            output = arg
+        else:
+            logging.info(f"{arg} has no further use from here on.")
+    
+    run(input=input, lexical_index=lexical_index, output=output)
