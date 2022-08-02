c71d12f94ee800455e8ccd20b765c95cc945b4cc
Harshad Hegde <hegdehb@gmail.com>
2022-08-02 11:24:42 -0500

    Filtered prefixes by `subject_prefixes`

--- src/scripts/match-mondo-sources-all-lexical.py
+++ src/scripts/match-mondo-sources-all-lexical.py
@@ -12,6 +12,7 @@
 
 import logging
 from pathlib import Path
+from typing import List
 from oaklib.resource import OntologyResource
 from oaklib.implementations.sqldb.sql_implementation import SqlImplementation
 from oaklib.utilities.lexical.lexical_indexer import (
@@ -20,15 +21,20 @@ from oaklib.utilities.lexical.lexical_indexer import (
     load_mapping_rules,
     save_lexical_index
 )
+import pandas as pd
 from sssom.writers import write_table
 from sssom.io import get_metadata_and_prefix_map
 import sys
 from pathlib import Path
 import click
+import yaml
+from sssom.constants import SUBJECT_ID, OBJECT_ID
+from sssom.util import KEY_FEATURES, get_prefix_from_curie
 
 SRC = Path(__file__).resolve().parents[1]
 ONTOLOGY_DIR = SRC / "ontology"
 OUT_INDEX_DB = ONTOLOGY_DIR / "tmp/merged.db.lexical.yaml"
+# KEY_FEATURES = [SUBJECT_ID, OBJECT_ID]
 
 input_argument = click.argument("input", required=True, type=click.Path())
 output_option = click.option(
@@ -72,6 +78,10 @@ def run(input:str, config: str, rules:str, output: str):
     # with open(config, "r") as f:
     #     sssom_yaml = yaml.safe_load(f)
     meta = get_metadata_and_prefix_map(config)
+    with open(config, "r") as f:
+        yml = yaml.safe_load(f)
+
+    prefix_of_interest = yml['subject_prefixes']
 
     resource = OntologyResource(slug=f"sqlite:///{Path(input).absolute()}")
     oi = SqlImplementation(resource=resource)
@@ -85,11 +95,37 @@ def run(input:str, config: str, rules:str, output: str):
     
     # msdf.prefix_map = sssom_yaml['curie_map']
     # msdf.metadata = sssom_yaml['global_metadata']
+    msdf.df = filter_prefixes(df=msdf.df, filter_prefixes=prefix_of_interest, features=[SUBJECT_ID, OBJECT_ID])
+    msdf.clean_prefix_map()
+    import pdb; pdb.set_trace()
 
     with open(str(SRC / Path(output)), "w", encoding="utf8") as f:
         write_table(msdf, f)
 
+# TODO: Replace the method below by sssom.utils.filter_prefixes.
+def filter_prefixes(
+    df: pd.DataFrame, filter_prefixes: List[str], features: list = KEY_FEATURES
+) -> pd.DataFrame:
+    """Filter any row where a CURIE in one of the key column uses one of the given prefixes.
 
+    :param df: Pandas DataFrame
+    :param filter_prefixes: List of prefixes
+    :param features: List of dataframe column names dataframe to consider
+    :return: Pandas Dataframe
+    """
+    filter_prefix_set = set(filter_prefixes)
+    rows = []
+
+    for _, row in df.iterrows():
+        prefixes = {get_prefix_from_curie(curie) for curie in row[features]}
+        # Confirm if all of the CURIEs in the list above appear in the filter_prefixes list.
+        # If TRUE, append row.
+        if all(prefix in filter_prefix_set for prefix in prefixes):
+            rows.append(row)
+    if rows:
+        return pd.DataFrame(rows)
+    else:
+        return pd.DataFrame(columns=features)
 
 if __name__ == '__main__':
     main()
