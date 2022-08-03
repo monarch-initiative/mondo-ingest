12d540f7910a54de76db3256a9aa89bac543f406
Harshad Hegde <hegdehb@gmail.com>
2022-08-03 16:26:34 -0500

    added filter msdf

--- src/scripts/match-mondo-sources-all-lexical.py
+++ src/scripts/match-mondo-sources-all-lexical.py
@@ -12,7 +12,7 @@
 
 import logging
 from pathlib import Path
-from typing import List
+from typing import List, TextIO
 from oaklib.resource import OntologyResource
 from oaklib.implementations.sqldb.sql_implementation import SqlImplementation
 from oaklib.utilities.lexical.lexical_indexer import (
@@ -22,15 +22,17 @@ from oaklib.utilities.lexical.lexical_indexer import (
     save_lexical_index
 )
 import pandas as pd
-from sssom.writers import write_table
-from sssom.io import get_metadata_and_prefix_map
+
 import sys
 from pathlib import Path
 import click
 import yaml
+
 from sssom.constants import SUBJECT_ID, OBJECT_ID
-from sssom.util import KEY_FEATURES, get_prefix_from_curie
+from sssom.util import KEY_FEATURES, get_prefix_from_curie, MappingSetDataFrame
 from sssom.parsers import parse_sssom_table
+from sssom.writers import write_table
+from sssom.io import get_metadata_and_prefix_map, run_sql_query
 
 SRC = Path(__file__).resolve().parents[1]
 ONTOLOGY_DIR = SRC / "ontology"
@@ -117,10 +119,20 @@ def run(input:str, config: str, rules:str, output: str):
         )
     msdf.df = msdf.df[msdf.df.columns.drop(list(msdf.df.filter(regex=r"_2")))]
     msdf.clean_prefix_map()
-    
 
     with open(str(SRC / Path(output)), "w", encoding="utf8") as f:
         write_table(msdf, f)
+
+    objects = msdf.df[OBJECT_ID].drop_duplicates()
+    prefixes = objects.str.split(":").str.get(0).drop_duplicates()
+    prefix_args = tuple([x+":%" for _, x in prefixes.iteritems() if x != "MONDO"])
+    kwargs = {"subject_id": ("MONDO:%",), "object_id": prefix_args}
+    with open(str(SRC / Path(output.replace("lexical", "lexical-2"))), "w") as f:
+        filter_file(
+            input=str(SRC / Path(output)),
+            output=f,
+            **kwargs
+            )
 
 # TODO: Replace the method below by sssom.utils.filter_prefixes.
 def filter_prefixes(
@@ -146,6 +158,49 @@ def filter_prefixes(
         return pd.DataFrame(rows)
     else:
         return pd.DataFrame(columns=features)
+
+def filter_file(input: str, output: TextIO, **kwargs) -> MappingSetDataFrame:
+    """Filter a dataframe by dynamically generating queries based on user input.
+
+    e.g. sssom filter --subject_id x:% --subject_id y:% --object_id y:% --object_id z:% tests/data/basic.tsv
+
+    yields the query:
+
+    "SELECT * FROM df WHERE (subject_id LIKE 'x:%'  OR subject_id LIKE 'y:%')
+     AND (object_id LIKE 'y:%'  OR object_id LIKE 'z:%') " and displays the output.
+
+    :param input: DataFrame to be queried over.
+    :param output: Output location.
+    :param **kwargs: Filter options provided by user which generate queries (e.g.: --subject_id x:%).
+    :raises ValueError: If parameter provided is invalid.
+    :return: Filtered MappingSetDataFrame object.
+    """
+    params = {k: v for k, v in kwargs.items() if v}
+    query = "SELECT * FROM df WHERE ("
+    multiple_params = True if len(params) > 1 else False
+
+    # Check if all params are legit
+    input_df: pd.DataFrame = parse_sssom_table(input).df
+    if not input_df.empty and len(input_df.columns) > 0:
+        column_list = list(input_df.columns)
+    else:
+        raise ValueError(f"{input} is either not a SSSOM TSV file or an empty one.")
+    legit_params = all(p in column_list for p in params)
+    if not legit_params:
+        invalids = [p for p in params if p not in column_list]
+        raise ValueError(f"The params are invalid: {invalids}")
+
+    for idx, (k, v) in enumerate(params.items(), start=1):
+        query += k + " LIKE '" + v[0] + "' "
+        if len(v) > 1:
+            for exp in v[1:]:
+                query += " OR "
+                query += k + " LIKE '" + exp + "') "
+        else:
+            query += ") "
+        if multiple_params and idx != len(params):
+            query += " AND ("
+    return run_sql_query(query=query, inputs=[input], output=output)
 
 if __name__ == '__main__':
     main()
