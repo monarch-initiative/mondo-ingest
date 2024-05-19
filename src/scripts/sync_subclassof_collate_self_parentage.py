62e0f60cfcb85fe69eba2a40f2da6c7ee1f63308
Joe Flack <joeflack4@gmail.com>
2024-05-18 23:31:46 -0400

    Subclass Sync: Self-parentage - Update: Several goals / overall structure of dependencies for subclass sync goals, which may include potential bug fixes. Though the structure is still a little confusing and deviates from other pipeline patterns in mondo-ingest, it should be less confusing now. - Update: Changed output dir of report from reports/ to tmp/ - Update: Changed how the self parentage report is generated. No longer doing appending. It is done in the same style as the 'direct-in-mondo-only' collation report. Less error prone.

--- src/scripts/sync_subclassof_collate_self_parentage.py
+++ src/scripts/sync_subclassof_collate_self_parentage.py
@@ -1,10 +1,11 @@
-"""Sync SubClass: Update report for Mondo classes having themselves as parents in the 'added' template."""
+"""Sync SubClass: Report for Mondo classes having themselves as parents in the "added" template (legal proxy merges)."""
 import logging
 import os
 import sys
 from argparse import ArgumentParser
+from glob import glob
 from pathlib import Path
-from typing import Dict
+from typing import Dict, List
 
 import pandas as pd
 
@@ -12,39 +13,61 @@ HERE = Path(os.path.abspath(os.path.dirname(__file__)))
 SRC_DIR = HERE.parent
 PROJECT_ROOT = SRC_DIR.parent
 sys.path.insert(0, str(PROJECT_ROOT))
-from src.scripts.sync_subclassof_config import OUTPATH_SELF_PARENTAGE, EX_DEFAULTS
+from src.scripts.sync_subclassof_config import COLLATE_SELF_PARENTAGE_DEFAULT_PATH, EX_DEFAULTS, \
+    SELF_PARENTAGE_DEFAULT_FILE_STEM, TMP_DIR
+DESC = 'Generating report for Mondo classes having themselves as parents in the "added" template (legal proxy merges).'
+
+
+def aggregate_inputs() -> pd.DataFrame:
+    """Aggregate all input files for each source
+    todo: this code is very similar to what's in the other collation script, so I'd like to abstract if possible at
+     some point. Some differences that the other script has, which could be dealt w/ via parameterization:
+      - deletes 2 columns in each loopp
+      - merged field names differ
+    """
+    df = pd.DataFrame()
+    files: List[str] = glob(str(TMP_DIR / f'*{SELF_PARENTAGE_DEFAULT_FILE_STEM}'))
+    if not files:
+        return df
+
+    for file in files:
+        df_i = pd.read_csv(file, sep='\t')
+        # Set initial dataframe for which subsequent ones will be joined
+        if len(df) == 0:
+            df = df_i
+            continue
+        # Concatenate data
+        df = pd.concat([df, df_i])
+    return df
 
 
 def collate_self_parentage(
-        mondo_mappings_path: str = EX_DEFAULTS['mondo_mappings_path'], in_outpath: str = OUTPATH_SELF_PARENTAGE
+    mondo_mappings_path: str = EX_DEFAULTS['mondo_mappings_path'], outpath: str = COLLATE_SELF_PARENTAGE_DEFAULT_PATH
 ):
-    """Update report for Mondo classes having themselves as parents in the 'added' template."""
-    logging.info('Updateing report for Mondo classes having themselves as parents in the "added" template.')
-
-    sssom_df = pd.read_csv(mondo_mappings_path, sep='\t', comment='#')
-    id_label_map: Dict[str, str] = dict(zip(sssom_df['object_id'], sssom_df['object_label']))
-
-    df = pd.read_csv(in_outpath, sep='\t').rename(columns={
-        'subject_mondo_id': 'mondo_id', 'subject_mondo_label': 'mondo_label'})
-    df['subject_source_label'] = df['subject_source_id'].map(id_label_map)
-    df['object_source_label'] = df['object_source_id'].map(id_label_map)
-    df = df[['mondo_id', 'mondo_label', 'subject_source_id', 'subject_source_label', 'object_source_id', 'object_source_label']]
-
-    df.to_csv(in_outpath, sep='\t', index=False)
+    """'Report for Mondo classes having themselves as parents in the "added" template (legal proxy merges)."""
+    logging.info(DESC)
+    df: pd.DataFrame = aggregate_inputs()
+    if len(df) == 0:
+        logging.warning(
+            'collate_self_parentage():'
+            'No inputs detected for collation of "self-parentage" source cases. This is probably the result of '
+            'running this step out of order. This should come at the end of the synchronization (subclass) pipeline, '
+            'after outputs have been generated for each class.')
+        return
+    df.to_csv(outpath, sep='\t', index=False)
 
 
 def cli():
     """Command line interface."""
     parser = ArgumentParser(
-        prog='sync-subclassof: Collate self-parentage issues',
-        description='Update report for Mondo classes having themselves as parents in the "added" template.')
-
+        prog='sync-subclassof: Collate self-parentage cases',
+        description='For: ' + DESC)
     parser.add_argument(
         '-m', '--mondo-mappings-path', required=False, default=EX_DEFAULTS['mondo_mappings_path'],
         help='Path to file containing all known Mondo mappings, in SSSOM format.')
     parser.add_argument(
-        '-o', '--in-outpath', required=False, default=OUTPATH_SELF_PARENTAGE,
-        help='The file containing all of the cases. It will be read as input and updated / saved again.')
+        '-o', '--outpath', required=False,
+        default=TMP_DIR / 'sync-subClassOf.added.self-parentage.tsv', help='Path to save output.')
     d: Dict = vars(parser.parse_args())
     collate_self_parentage(**d)
 
