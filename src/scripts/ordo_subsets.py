c66b7afe10d0dfeb346cb4dc5b168ec2cc6412f2
Joe Flack <joeflack4@gmail.com>
2024-05-24 12:16:24 -0400

    Update src/scripts/ordo_subsets.py
    Rename ORDO subset URI ordo_disease --> ordo_disorder
    
    Co-authored-by: Nico Matentzoglu <nicolas.matentzoglu@gmail.com>

--- src/scripts/ordo_subsets.py
+++ src/scripts/ordo_subsets.py
@@ -7,7 +7,7 @@ from utils import remove_angle_brackets
 
 
 SUBSET_MAP = {
-    'Orphanet:557493': 'http://purl.obolibrary.org/obo/mondo#ordo_disease',  # disorder
+    'Orphanet:557493': 'http://purl.obolibrary.org/obo/mondo#ordo_disorder',  # disorder
     'Orphanet:557492': 'http://purl.obolibrary.org/obo/mondo#ordo_group_of_disorders',  # group of disorders
     'Orphanet:557494': 'http://purl.obolibrary.org/obo/mondo#ordo_subtype_of_a_disorder',  # subtype of a disorder
 }
