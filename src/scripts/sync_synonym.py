943480b29cda45a64838a2045dd74a44b8f3d6dc
Joe Flack <joeflack4@gmail.com>
2024-06-19 20:41:18 -0400

    Synonym synchronization - Refactor: Previous setup was for general sync; changing to be specific to synonyms. Undid any general changes; either that or they have already been done separately and rebased.

--- src/scripts/sync_synonym.py
+++ src/scripts/sync_synonym.py
@@ -1,132 +1,170 @@
-"""Ontology synchronization
-# Resources
-- GitHub issue: https://github.com/monarch-initiative/mondo-ingest/issues/87
+"""Whenever new synonym or label in upstream source, want to add it to Mondo. If the label is not the Mondo label or
+existing synonym, it will become a synonym.
 """
-import os
 from argparse import ArgumentParser
+from datetime import datetime
+from pathlib import Path
+from typing import Union, List, Tuple, Dict, Set
 
 import pandas as pd
-from oaklib.implementations import ProntoImplementation
+from oaklib import get_adapter
+from oaklib.datamodels.ontology_metadata import Annotation
+from oaklib.implementations import SqlImplementation
+from oaklib.types import CURIE, PRED_CURIE
 
-# Vars
-SCRIPTS_DIR = os.path.dirname(os.path.realpath(__file__))
-PROJECT_DIR = os.path.join(SCRIPTS_DIR, '..', '..')
-ONTOLOGY_DIR = os.path.join(PROJECT_DIR, 'src', 'ontology')
-# REPORTS_DIR = os.path.join(ONTOLOGY_DIR, 'reports')
-# METADATA_DIR = os.path.join(ONTOLOGY_DIR, 'metadata')
-# CONFIG_DIR = os.path.join(ONTOLOGY_DIR, 'config')
-SYNC_DIR = os.path.join(ONTOLOGY_DIR, 'sync')
-TEMP_DIR = os.path.join(ONTOLOGY_DIR, 'tmp')
-CACHE_DIR = TEMP_DIR
+from src.scripts.utils import PREFIX_MAP, get_owned_prefix_map
 
 
-# TODO: these funcs obsolete
-# Functions
-# TODO: 1 func for each step. do they have sep outputs?
-def sync_subclassof_axioms():
-    """Synchronizes subClassOf axioms."""
-    pass
+# todo: reactivate if needed
+# HERE = Path(os.path.abspath(os.path.dirname(__file__)))
+# SRC_DIR = HERE.parent
+# PROJECT_ROOT = SRC_DIR.parent
+# sys.path.insert(0, str(PROJECT_ROOT))
+# from src.scripts.utils import ONTOLOGY_DIR
 
 
-def sync_xrefs():
-    """Synchronizes cross-references."""
-    pass
+def _get_synonyms(ids: List[CURIE], db: SqlImplementation) -> pd.DataFrame:
+    """Get synonym triples from sqlite DB"""
+    # todo: del this commented out stuff at end. leaving here in case I decide to change impl back from set to dict
+    # id_syns_map: Dict[CURIE, List[Tuple[PRED_CURIE, str]]] = {_id: [] for _id in ids}
+    # for _id in ids:
+    #     for pred, alias in sorted(db.alias_relationships(_id)):
+    #         id_syns_map[_id].append((pred, alias))
 
-
-def sync_definitions():
-    """Synchronizes term definitions."""
-    pass
-
-
-def sync_obsoletion():
-    """Synchronizes term obsoletions."""
-    pass
-
-
-# TODO: If synonym is not already in Mondo, add it into a sync/%-synonyms.robot.template.tsv
-def sync_synonyms(ontology: ProntoImplementation, mondo: ProntoImplementation, outpath_synonyms: str, cache=False):
-    """Whenever new synonym or label in upstream source, want to add it to Mondo. If the label is not the Mondo label or
-     existing synonym, it will become a synonym."""
-    # todo: params: ontology,mondo: Can OAK support synonyms / other needs? Or should I use sparql?
-    print()
+    tups: Set[Tuple[CURIE, PRED_CURIE, str]] = set()
+    for _id in ids:
+        for pred, alias in sorted(db.alias_relationships(_id)):
+            # - filter all but synonyms: prior in pipeline have already converted other props such as 'label' to syns
+            # todo: is this the best filter? is there more that can appear that should be filtered out? Should instead
+            #  not filter by things we want out, but by the list of synonym preds we want in?
+            if pred == 'rdfs:label':
+                continue
+            tups.add((_id, pred, alias))
+    df = pd.DataFrame(tups, columns=['curie', 'synonym_scope', 'synonym'])
+    return df
 
 
 # TODO
-def run(
-    ontology_path: str, mondo_path: str, mondo_sans_exclusions_path: str, sssom_map_path: str, onto_config_path: str,
-    outpath_synonyms: str, outpath: str, cache=False
+def sync_synonyms(
+    ontology_db_path: Union[Path, str], mondo_db_path: Union[Path, str], mondo_mappings_path: Union[Path, str],
+    onto_config_path: Union[Path, str], outpath_added: Union[Path, str], outpath_updated: Union[Path, str]
 ):
-    """Run"""
-    # Prefixes
-    # TODO: read ontology_path (repurpose from slurp) (cache it? dk if it really saves time. can time it)
-    ontology = None
-    # TODO: read mondo_path
-    mondo = None
-    # TODO: read mondo_sans_exclusions_path (do I even need mondo.owl if using this?)
-    mondo_sans_axiom_exclusions = None
-    # TODO: read sssom_map_path
-    sssom_df = pd.DataFrame()
-    # TODO: onto_config_path: Needed?
-    # with open(config_path, 'r') as stream:
-    #     onto_config = yaml.safe_load(stream)
-    # prefix_uri_map = onto_config['base_prefix_map']
+    """Whenever new synonym or label in upstream source, want to add it to Mondo. If the label is not the Mondo label or
+     existing synonym, it will become a synonym."""
+    # Iterate over mappings
+    mappings_df = pd.read_csv(mondo_mappings_path, sep='\t', comment='#')
+    # todo: refactor filtering by prefixes
+    #  check if this pattern has been used elsewhere; I think it has. and abstract into func for these 3 lines; 'filter prefixes'
+    #  - OR use this?: get_all_owned_terms()
+    #    - it seems long and messy compared to these 3 lines, but is more comprehensive. I could make get_all_owned_terms() a simple 3 line func and then make a separate func for all edge cases?
+    # - filter by source
+    owned_prefix_map: PREFIX_MAP = get_owned_prefix_map(onto_config_path)
+    mappings_df['object_prefix'] = mappings_df['object_id'].apply(lambda x: x.split(':')[0])
+    mappings_df = mappings_df[mappings_df['object_prefix'].isin(owned_prefix_map.keys())]
+    del mappings_df['object_prefix']
 
-    # TODO:
-    # todo: @nico: Clarify; says 'axioms', but does this apply to xrefs, defs, obsoletions, synonyms?
-    # Algo
-    # https://github.com/monarch-initiative/mondo-ingest/issues/27
-    # 1. Remove all support from O_t in all axioms in O_s
-    # 2. Import all axioms from O_t that is not in O_s or O_reject.
-    # 3. Add support for all axioms in O_s that are in O_t.
-    # 4. Human reviewer looks at all new axioms O_s (O_t-(O_s+O_reject)).
-    #    - If the axiom is bad, add the axiom to (O_reject)
-    # 5. Repeat process (start from 1) until all axioms are good (if all bad axioms are removed in the first run, just
-    #    one second run is needed).
-    # 6. Optionally, human reviewer looks at all axioms in O_t that now lost all support.
-    #    - If the axiom is bad, add the axiom to (O_reject)
-    # 7. (reduce is not run) (kgcl/sssom cross-product)
-    sync_synonyms(ontology=ontology, mondo=mondo, outpath_synonyms=outpath_synonyms, cache=cache)  # TODO
-    sync_subclassof_axioms()  # todo
-    sync_xrefs()  # todo
-    sync_definitions()  # todo
-    sync_obsoletion()  # todo
-    print(outpath)  # todo: write update?
+    # Query synonyms
+    print(f'Querying synonyms')
+    # - Source
+    # todo: do I need the db's elsewhere? if not, refactor to instantiate them inside the func
+    source_db: SqlImplementation = get_adapter(ontology_db_path)
+    t0 = datetime.now()
+    source_df: pd.DataFrame = _get_synonyms(mappings_df['object_id'].tolist(), source_db)
+    source_df = source_df.rename(columns={'curie': 'source_id'})
+    source_df['mondo_id'] = ''  # todo: do I want to add this at this stage?
+    source_df = source_df[['mondo_id', 'synonym_scope', 'synonym', 'source_id']]  # todo: do I want to add this at this stage?
+    t1 = datetime.now()
+    print(f'- queried source in n seconds: {(t1 - t0).seconds}')
+    # - Mondo
+    mondo_db: SqlImplementation = get_adapter(mondo_db_path)
+    mondo_df: pd.DataFrame = _get_synonyms(mappings_df['subject_id'].tolist(), mondo_db)
+    mondo_df = mondo_df.rename(columns={'curie': 'mondo_id'})
+    t2 = datetime.now()
+    print(f'- queried Mondo (basic info) in n seconds: {(t2 - t1).seconds}')
+    #  - fetch sources for Mondo synonyms
+    # TODO: fetch synonym source
+    # todo: might want to consider subsetting like 100 rows just for demo
+    # todo: pr comment: performance _axiom_annotations(): 20k rows, while mondo_df had 56k rows in 15-20 min. although set of mondo ID in these rows was 6509 while in mondo_df was 9706
+    rows2: List[Tuple[CURIE, PRED_CURIE, str, CURIE]] = []
+    i = 0
+    for row in mondo_df.itertuples():
+        t2_i = datetime.now()
+        i += 1
+        # noinspection PyProtectedMember,PyUnresolvedReferences doesnt_detect_named_tuples
+        anns: List[Annotation] = [x for x in mondo_db._axiom_annotations(row.mondo_id, row.synonym_scope, value=row.synonym)]
+        # noinspection PyUnresolvedReferences doesnt_detect_named_tuples
+        rows2.extend([(row.mondo_id, row.synonym_scope, row.synonym, ann.object) for ann in anns])
+        if i % 50 == 0:
+            print(f'queried {i} of {len(mondo_df)} Mondo IDs in n seconds: {(datetime.now() - t2_i).seconds}')
+
+    # todo: based on performance, consider: _axiom_annotations_multi(
+    # todo: address sqlalchemy.exc.OperationalError: (sqlite3.OperationalError) too many SQL variables
+    rows2: List[Tuple[CURIE, PRED_CURIE, str, CURIE]] = []
+    mondo_ids: List[CURIE] = mondo_df['mondo_id'].tolist()
+    batch_size = 5000  # addresses: (sqlite3.OperationalError) too many SQL variables
+    preds = mondo_df['synonym_scope'].unique()
+    i = 0
+    for pred in preds:
+        i += 1
+        for j in range(0, len(mondo_ids), batch_size):
+            t2_i = datetime.now()
+            mondo_ids_subset = mondo_ids[j:j + batch_size]
+            # noinspection PyProtectedMember
+            anns: List[Annotation] = [x for x in mondo_db._axiom_annotations_multi(
+                mondo_ids_subset, pred,
+                # values=mondo_df['synonym'].tolist()
+            )]
+            print(f'- queried Mondo (synonym sources), predicate {i} of {len(preds)}, {batch_size} Mondo IDs ({j+batch_size} so far of {len(mondo_ids)}) in n seconds: {(datetime.now() - t2_i).seconds}')
+            # noinspection PyUnresolvedReferences doesnt_detect_named_tuples
+            rows2.extend([(ann.subject, pred, ann.value, ann.object) for ann in anns])  # TODO: this work?
+
+    t3 = datetime.now()
+    print(f'- queried Mondo (synonym sources) in n seconds: {(t3 - t2).seconds}')
+    mondo_df2 = pd.DataFrame(rows2, columns=['mondo_id', 'synonym_scope', 'synonym', 'source_id'])
+
+    # TODO: filter by source_id in owned prefixes
+    pass
+
+    # TODO: set diffs
+    print()
+
+    print()
+    # TODO: sources for mondo synonyms
+    #  - we can get this via sparql. but can we get via OAK?
+    #  - this will help to add source to existing synonyms from other sources, or remove from specific sources
+    # TODO: synonym type, e.g. DEPRACATED
 
 
+
+# todo: #1 make sure that the help text, short flags, etc is consistent etween files
 def cli():
     """Command line interface."""
     package_description = \
         'Ontology synchronization pipeline. Automates integration of updates from other ontologies into Mondo.'
     parser = ArgumentParser(description=package_description)
+    # TODO: Which way is better? (a) mondo-ingest.db, as subclass sync was doing? or (b) the individual source's.db?
+    #  I think 'b' is better but IDR why I set up subclass sync w/ 'a'.
     parser.add_argument(
-        '-O', '--ontology-path', required=True,
-        help='Path to the ontology file to sync from into Mondo.')
+        '-d', '--ontology-db-path', required=False,
+        help='Path to SemanticSQL sqlite `.db` file for the given source ontology.')
     parser.add_argument(
-        '-m', '--mondo-path', required=True,
-        help='Path to `mondo.owl`.')
-    parser.add_argument(  # todo: change name to mondo-sans-axiom-exclusions-path? If so: update makefile, debug configs
-        '-e', '--mondo-sans-exclusions-path', required=True,
-        help='Path to a copy of Mondo that excludes certain axioms.')
+        '-D', '--mondo-db-path', required=False,
+        help='Path to SemanticSQL sqlite `mondo.db`.')
     parser.add_argument(
-        '-s', '--sssom-map-path', required=True,
+        '-s', '--mondo-mappings-path', required=True,  # TODO: Is this arg needed?
         help='Path to file containing all known Mondo mappings, in SSSOM format.')
-    # TODO: Is this needed?
     parser.add_argument(
-        '-c', '--onto-config-path', required=True,
+        '-c', '--onto-config-path', required=True,  # TODO: Is this arg needed?
         help='Path to a config `.yml` for the ontology which contains a `base_prefix_map` which contains a '
              'list of prefixes owned by the ontology. Used to filter out terms.')
     parser.add_argument(
-        '-os', '--outpath-synonyms', required=True,
-        help='Path to robot template TSV to create whicih will contain synonyms that aren\'t yet integrated into Mondo '
+        '-a', '--outpath-added', required=True,
+        help='Path to robot template TSV to create which will contain synonyms that aren\'t yet integrated into Mondo '
              'for given ontoogy.')
-    # TODO: will this be the same as --mondo-path?
     parser.add_argument(
-        '-o', '--outpath', required=True,
-        help='Path to variation of `mondo.owl` to create/update.')
-    parser.add_argument(
-        '-C', '--cache', required=False,
-        help='Cache intermediaries? E.g. results of queries for a given ontology.')
-    run(**vars(parser.parse_args()))
+        '-u', '--outpath-updated', required=True,
+        help='Path to robot template TSV to create which will contain synonyms changes.')
+    sync_synonyms(**vars(parser.parse_args()))
 
 
 # Execution
