53717c52da27eb23bec415c6b95b549576827345
Joe Flack <joeflack4@gmail.com>
2024-07-22 18:21:11 -0400

    Synonym synchronization: Refactor to remove role of source_id in case conditions, bug fixes, tests, new outputs, and new evidence column. - Update: Deactivated -deleted template. - Update: -confirmed and -updated templates: don't care about source_id - Bug fix: ondo-synonyms-scope-type-xref.sparql: hasDbXref was not optional, meaning that synonyms without evidence were being ignored. - Add: evidence column - Add: ROBOT template outputs - Add: Tests
    General updates
    - Test setup: (i) update .gitignore to allow for .db files in test/input (they will be small). (ii) keep tests/output/ dir, but only the .gitkeep file in it.

--- src/scripts/sync_synonym.py
+++ src/scripts/sync_synonym.py
@@ -18,7 +18,20 @@ sys.path.insert(0, str(PROJECT_ROOT))
 from src.scripts.utils import PREFIX_MAP, get_owned_prefix_map
 
 
-NON_SOURCE_ID_SOURCE_SPECIFIC_SYNONYM_XREFS = ['OMIM:genemap2']  # current as of 2027/07/15
+# todo: when dirty_df no longer necessary, remove 'case' from these
+HEADERS_TO_ROBOT_SUBHEADERS = {
+    'mondo_id': 'ID',
+    'mondo_label': '',
+    'synonym_scope': 'AI',  # todo next impt: pred  - 'A' correct? AI?
+    'synonym': '',  # todo: obj of 'scope' ow?
+    'source_id': '>A oboInOwl:source',  # todo axiom  # todo question: This is how we annotate source CURIEs. but isn't a CURIE similar to an IRI, so why isn't it 'AI' insead of just 'A'?
+    'source_label': '',
+    'synonym_type': '',
+    'mondo_evidence': '',
+    'case': '',
+    'synonym_scope_mondo': '',
+}
+SORT_COLS = ['case', 'mondo_id', 'source_id', 'synonym_scope', 'synonym_type', 'synonym']
 
 
 def _query_synonyms(ids: List[CURIE], db: SqlImplementation) -> pd.DataFrame:
@@ -36,23 +49,50 @@ def _query_synonyms(ids: List[CURIE], db: SqlImplementation) -> pd.DataFrame:
     return df
 
 
-def _filter_non_intersecting_rows(
-        base_df: pd.DataFrame, intersecting_df: pd.DataFrame, join_on: List[str]
+def _format_and_save(
+    df: pd.DataFrame, outpath: Union[Path, str], order_cols: List[str] = list(HEADERS_TO_ROBOT_SUBHEADERS.keys()),
+        sort_cols: List[str] = SORT_COLS,
+    save=True
 ) -> pd.DataFrame:
-    """Filter out all rows are not in common, given a `join_on`."""
-    merge_df = base_df.merge(intersecting_df, on=join_on, how='left', indicator=True)
-    filtered_df = merge_df[merge_df['_merge'] == 'left_only'].drop(columns=['_merge'])
-    filtered_df = filtered_df.rename(
-        columns=lambda x: x[:-2] if x.endswith('_x') else x)[list(base_df.columns)]
-    return filtered_df
+    """Format column order and sorting, drop any superfluous columns, and optionally save to file."""
+    order_cols = [x for x in order_cols if x in df.columns]
+    sort_cols = [x for x in sort_cols if x in df.columns]
+    df = df[order_cols].sort_values(by=sort_cols)
+    if save:
+        df.to_csv(outpath, sep='\t', index=False)
+    return df
+
+
+def _filter_a_by_not_in_b(
+        df_a: pd.DataFrame, df_b: pd.DataFrame, join_on: List[str]
+) -> pd.DataFrame:
+    """Filter out all rows not in common, given a `join_on`.
+
+    Example use case: -added
+    - df_a: Dataframe of synonym data from a source
+    - df_b: Dataframe of synonym data from a source
+    - join_on: ['mondo_id', 'synonym'] - we want to find ones that are in source but not in Mondo
+
+    Alternative implementation yielding exact same results:
+        merge_df = base_df.merge(to_intersect_df, on=join_on, how='left', indicator=True)
+        filtered_df = merge_df[merge_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+        return filtered_df.rename(
+            columns=lambda x: x[:-2] if x.endswith('_x') else x)[list(base_df.columns)]
+    """
+    return df_a[~df_a[join_on].apply(tuple, axis=1).isin(
+        df_b[join_on].apply(tuple, axis=1)
+    )]
 
 
 def sync_synonyms(
     ontology_db_path: Union[Path, str], mondo_synonyms_path: Union[Path, str], mondo_mappings_path: Union[Path, str],
     onto_config_path: Union[Path, str], outpath_added: Union[Path, str], outpath_confirmed: Union[Path, str],
-    outpath_deleted: Union[Path, str], outpath_updated: Union[Path, str]
+    outpath_deleted: Union[Path, str], outpath_updated: Union[Path, str], deactivate_deleted=True,
+    combined_outpath_template_str = 'tmp/synonym_sync_combined_cases_{}.tsv'
 ):
     """Create outputs for syncing synonyms between Mondo and its sources.
+
+    :param deactivate_deleted: If True, will not create the '-deleted' template.
 
     todo: possible refactor: labels: Maybe could be done more cleanly and consistently. At first, wanted to add to both
      source_df and mondo_df, but this caused _x and _y cols during joins, or I would have to join on those cols as well.
@@ -71,103 +111,131 @@ def sync_synonyms(
     mappings_df = mappings_df.rename(columns={'subject_id': 'mondo_id', 'subject_label': 'mondo_label',
           'object_id': 'source_id', 'object_label': 'source_label'})
     # - remove unneeded cols
-    mappings_df = mappings_df[['mondo_id', 'mondo_label', 'source_id', 'source_label']]
-    # todo: refactor filtering by prefixes
+    mappings_df = mappings_df[['mondo_id', 'mondo_label', 'source_id']]  # source_label added later from source
+
+    # - filter by source
+    # todo: filtering by prefixes / source
+    # Todo: i. keep or remove?
+    #   the role of source_id in -confirmed and -updated is different than what I originally thought. Do I still need to
+    #   filter here? I think so. Originally our pseudocode was an iterator over only these matches, I thought
+    # todo: ii. if keep: refactor
     #  check this pattern used elsewhere; I think it has. and abstract into func for these 3 lines; 'filter prefixes'
     #  - OR use this?: get_all_owned_terms()
     #    - it seems long and messy compared to these 3 lines, but is more comprehensive. I could make
     #      get_all_owned_terms() a simple 3 line func and then make a separate func for all edge cases?
     #  - note I may have list(owned_prefix_map.keys())[0] instead of list(owned_prefix_map)[0] ins ome places
-    # - filter by source
     mappings_df['source_prefix'] = mappings_df['source_id'].apply(lambda x: x.split(':')[0])
     mappings_df = mappings_df[mappings_df['source_prefix'].isin(owned_prefix_map.keys())]
     del mappings_df['source_prefix']
+
     # - filter obsoletes (from mappings_df --> source_df & mondo_df)
     #  - filter obsoleted in Mondo
     mappings_df = mappings_df[~mappings_df['mondo_label'].str.startswith('obsolete')]
+    # Todo: obsoletes/deleted filtering correct?
+    #  I feel like this way might be wrong. this is probably too early / not the correct place to be filtering
     #  - filter obsoleted in source
     source_obsoletes: Set[CURIE] = set([x for x in source_db.obsoletes()])
     mappings_df = mappings_df[~mappings_df['source_id'].isin(source_obsoletes)]
+    # - labels / label lookups
+    # todo: I'd like to move these labels somewhere different for better flow, but requires refactor
+    source_labels: List[Tuple[CURIE, str]] = [x for x in source_db.labels(mappings_df['source_id'].tolist())]
+    source_labels: Dict[CURIE, str] = {curie: label for curie, label in source_labels}
+    mappings_df['source_label'] = mappings_df['source_id'].map(source_labels)
+    mondo_labels: Dict[CURIE, str] = mappings_df.set_index('mondo_id')['mondo_label'].to_dict()
     # - filter mapped but deleted IDs
     #   If there is no label, we take that as a proxy / sign that it has been deleted. Alternative ways: entities(),
     #   all_entity_curies(), node() (empty props), node_exists() (no: falsely says 'True', prolly cuz bad xrefs exist)
-    source_labels: List[Tuple[CURIE, str]] = [x for x in source_db.labels(mappings_df['source_id'].tolist())]
-    source_deleted_ids: Set[CURIE] = set([x[0] for x in source_labels if not x[1]])
+    source_deleted_ids: Set[CURIE] = set([k for k, v in source_labels.items() if not v])
     mappings_df = mappings_df[~mappings_df['source_id'].isin(source_deleted_ids)]
     if len(mappings_df) == 0:
         logging.warning(f'Synonym Sync: No mappings found for source {source_name}. Exiting.')
         return
-    # - update source labels (sometimes Mondo & source have discrepancy)
-    source_labels: Dict[CURIE, str] = {curie: label for curie, label in source_labels}
-    mappings_df['source_label'] = mappings_df['source_id'].map(source_labels)
 
     # Query synonyms: source
-    source_df: pd.DataFrame = _query_synonyms(mappings_df['source_id'].tolist(), source_db)
-    # - add mapped mondo ids
-    source_mondo_lookup = mappings_df.set_index('source_id')['mondo_id'].to_dict()
-    source_df['mondo_id'] = source_df['curie'].map(source_mondo_lookup)
-    source_df = source_df.rename(columns={'curie': 'source_id'})[['mondo_id', 'source_id', 'synonym_scope', 'synonym']]
+    source_df: pd.DataFrame = _query_synonyms(mappings_df['source_id'].tolist(), source_db)\
+        .rename(columns={'curie': 'source_id'})
+    source_df['synonym_lower'] = source_df['synonym'].str.lower()
+    source_df['source_label'] = source_df['source_id'].map(source_labels)
 
     # Query synonyms: Mondo
-    mondo_df = pd.read_csv(mondo_synonyms_path, sep='\t').rename(columns={'?dbXref': 'source_id'})
-    mondo_df = mondo_df.rename(columns=lambda x: x.lstrip('?'))
-    # - filter by source
-    mondo_df['source_prefix'] = mondo_df['source_id'].apply(lambda x: x.split(':')[0])
-    mondo_df = mondo_df[mondo_df['source_prefix'].isin(owned_prefix_map.keys())]
-    del mondo_df['source_prefix']
-    # - filter by source previously filtered mappings_df (obsoletes & deletions)
-    mondo_df = mondo_df[mondo_df['mondo_id'].isin(set(mappings_df['mondo_id']))]  # alternative: JOIN-based solution
-    # - filter out rows which don't appear in mondo.sssom.tsv for whatever reason
-    mondo_df = mondo_df.merge(mappings_df[['mondo_id', 'source_id']], on=['mondo_id', 'source_id'], how='inner')
-    # - filter out any non-source-ID, source-specific synonym xrefs
-    #   Not future-proof, as this list has to be maintained, but as of 2024/07/15 there is only one entry, so there is
-    #   not high risk of this being a problem. Alternative: Come up w/ regex for valid codes for all sources. Will
-    #   mostly be numbers, but can include . and -. Not sure if can include alpha chars.
-    mondo_df = mondo_df[~mondo_df['source_id'].isin(NON_SOURCE_ID_SOURCE_SPECIFIC_SYNONYM_XREFS)]
+    mondo_df = pd.read_csv(mondo_synonyms_path, sep='\t').rename(columns={'?dbXref': 'source_id'}).fillna('')
+    mondo_df['source_id'] = mondo_df['source_id'].apply(lambda x: x.replace('https://orcid.org/', 'ORCID:'))
+    mondo_df['synonym_lower'] = mondo_df['synonym'].str.lower()
+    # - URI -> CURIEs
+    # todo: utilize curies package; handle more cases
+    mondo_df['source_id'] = mondo_df['source_id'].apply(lambda x: x.split('/')[-1] if x else '')
+    # - add evidence column
+    mondo_evidence_lookup: Dict[Tuple, List[str]] = mondo_df.groupby(
+        ['mondo_id', 'synonym_scope', 'synonym'])['source_id'].agg(list).to_dict()
+    mondo_evidence_lookup: Dict[Tuple, str] = {k: ', '.join(v) for k, v in mondo_evidence_lookup.items()}
+    mondo_evidence_rows: List[List[str]] = []
+    for k, v in mondo_evidence_lookup.items():
+        mondo_evidence_rows.append(list(k) + [v])
+    mondo_evidence_df = pd.DataFrame(
+        mondo_evidence_rows, columns=['mondo_id', 'synonym_scope', 'synonym', 'mondo_evidence'])
+    mondo_df = mondo_df.merge(mondo_evidence_df, on=['mondo_id', 'synonym_scope', 'synonym'], how='left')
+    # - only need 1 row per unique combo of ['mondo_id', 'synonym_scope', 'synonym']
+    del mondo_df['source_id']
+    mondo_df.drop_duplicates(inplace=True)
+    # - filter terms not in mondo.sssom.tsv
+    #   - also effectively filters by source previously filtered mappings_df (obsoletes & deletions)
+    mondo_df = mondo_df[mondo_df['mondo_id'].isin(set(mappings_df['mondo_id']))]
     # - add labels
-    mondo_df = mondo_df.merge(mappings_df, on=['mondo_id', 'source_id'], how='left')
+    mondo_df['mondo_label'] = mondo_df['mondo_id'].map(mondo_labels)
 
-    # Determine synchronization cases    # -confirmed
-    confirmed_df = mondo_df.merge(source_df, on=['mondo_id', 'source_id', 'synonym_scope', 'synonym'], how='inner')
+    # Determine synchronization cases
+    # -confirmed
+    #  Cases where scope + synonym string are the same
+    confirmed_df = mondo_df.merge(source_df, on=['synonym_scope', 'synonym_lower'], how='inner').rename(columns={
+        'synonym_x': 'synonym', 'synonym_y': 'synonym_source'})  # keep Mondo casing if different
+    del confirmed_df['mondo_evidence']
+    confirmed_df = _format_and_save(confirmed_df, outpath_confirmed)
     confirmed_df['case'] = 'confirmed'
 
     # -updated
-    updated_df = mondo_df.merge(source_df, on=['mondo_id',  'source_id', 'synonym',], how='inner').rename(
-        columns={'synonym_scope_x': 'synonym_scope_mondo', 'synonym_scope_y': 'synonym_scope'})
+    #  Cases where scope has is different in source
+    updated_df = mondo_df.merge(source_df, on=['synonym_lower'], how='inner').rename(columns={
+        'synonym_scope_x': 'synonym_scope_mondo', 'synonym_scope_y': 'synonym_scope',
+        'synonym_x': 'synonym', 'synonym_y': 'synonym_source'})  # keep Mondo casing if different
     updated_df = updated_df[updated_df['synonym_scope_mondo'] != updated_df['synonym_scope']]
-    updated_df = updated_df[[
-        'mondo_id', 'mondo_label', 'source_id', 'source_label', 'synonym_scope_mondo', 'synonym_scope', 'synonym',
-        'synonym_type']]
+    updated_df = _format_and_save(updated_df, outpath_updated)
     updated_df['case'] = 'updated'
 
     # -added
-    added_df = source_df.merge(
-        mondo_df, on=['mondo_id', 'source_id', 'synonym_scope', 'synonym'], how='left', indicator=True)
-    added_df = added_df[added_df['_merge'] == 'left_only'].drop(columns=['_merge', 'mondo_label', 'source_label'])
-    # - add labels
-    for ont in ['mondo', 'source']:
-        added_df = added_df.merge(
-            mappings_df[[f'{ont}_id', f'{ont}_label']].drop_duplicates(), on=f'{ont}_id', how='left')
-    added_df = _filter_non_intersecting_rows(added_df, updated_df, ['mondo_id', 'source_id', 'synonym'])
-    del added_df['synonym_type']
+    #  Cases where synonym exists in source term but not in mapped Mondo term
+    # - add mapped mondo IDs
+    source_df_with_mondo_ids = source_df.merge(mappings_df[['source_id', 'mondo_id', 'mondo_label']], on=['source_id'], how='inner')
+    # - leave only synonyms that don't exist on given Mondo IDs
+    added_df = _filter_a_by_not_in_b(source_df_with_mondo_ids, mondo_df, ['mondo_id', 'synonym_lower'])
+    added_df = _format_and_save(added_df, outpath_added)
     added_df['case'] = 'added'
 
     # -deleted
-    deleted_df = mondo_df.merge(
-        source_df, on=['mondo_id', 'source_id', 'synonym_scope', 'synonym'], how='left', indicator=True)
-    deleted_df = deleted_df[deleted_df['_merge'] == 'left_only'].drop('_merge', axis=1)  # also can do via mondo_id=nan
-    deleted_df = _filter_non_intersecting_rows(deleted_df, updated_df, ['mondo_id', 'source_id', 'synonym'])
-    deleted_df['case'] = 'deleted'
+    #  Cases where synonym exists in Mondo term, but not in mapped source term
+    deleted_df = pd.DataFrame()
+    # todo: reactivate when ready.
+    #  - depends on more than just 1 source not having synonym. it must (i) exist on no mapped source terms, and (ii) have
+    #  no other qualifying evidence (I think just: ORCID & MONDO;notVerified),
+    #  - considerations: what role source_id plays in this logic. this may be outdated
+    #  - ensure has labels
+    # todo: do 100% of mondo and source terms in here have labels? I think they should.
+    if not deactivate_deleted:
+        # todo: i think this implementation is outdated post source_id refactor
+        deleted_df = mondo_df.merge(
+            source_df, on=['synonym_scope', 'synonym_lower'], how='left', indicator=True)
+        deleted_df = deleted_df[deleted_df['_merge'] == 'left_only'].drop('_merge', axis=1)  # also can do via mondo_id=nan
+        deleted_df = _filter_a_by_not_in_b(deleted_df, updated_df, ['mondo_id', 'source_id', 'synonym_lower'])
+        deleted_df = _format_and_save(deleted_df, outpath_deleted)
+        deleted_df['case'] = 'deleted'
 
     # Write outputs
     # todo: temp: dirty_df: combine all cases for analysis during development
-    dirty_df = pd.concat([confirmed_df, added_df, updated_df, deleted_df], ignore_index=True)[[
-        'mondo_id', 'mondo_label', 'source_id', 'source_label', 'synonym_scope', 'synonym', 'synonym_type', 'case',
-        'synonym_scope_mondo']]\
-        .sort_values(by=['case', 'mondo_id', 'source_id', 'synonym_scope', 'synonym_type', 'synonym'])
-    dirty_df['source_ontology'] = source_name
-    dirty_df = dirty_df.rename(columns={'synonym_scope_mondo': 'synonym_scope_mondo__for_-updated_case'})
-    dirty_df.to_csv(f'tmp/synonym_sync_combined_cases_{source_name}.tsv', sep='\t', index=False)
+    if combined_outpath_template_str:
+        dirty_df = pd.concat([confirmed_df, added_df, updated_df, deleted_df], ignore_index=True)
+        dirty_outpath = str(combined_outpath_template_str).format(source_name)
+        dirty_df = _format_and_save(dirty_df, dirty_outpath)
+        dirty_df['source_ontology'] = source_name
+        dirty_df.to_csv(dirty_outpath, sep='\t', index=False)
 
 
 def cli():
@@ -190,17 +258,20 @@ def cli():
              'list of prefixes owned by the ontology. Used to filter out terms.')
     parser.add_argument(
         '-a', '--outpath-added', required=True,
-        help='Path to robot template TSV to create which will contain synonyms that aren\'t yet integrated into Mondo '
-             'for given ontoogy.')
+        help='Path to ROBOT template TSV to create which will contain synonyms that aren\'t yet integrated into Mondo '
+             ' for all mapped source terms.')
     parser.add_argument(
         '-c', '--outpath-confirmed', required=True,
-        help='Path to robot template TSV to create which will contain synonym confirmations.')
+        help='Path to ROBOT template TSV to create which will contain synonym confirmations; combination of synonym '
+             'scope predicate and synonym string exists in both source and Mondo for a given mapping.')
     parser.add_argument(
         '-d', '--outpath-deleted', required=True,
-        help='Path to robot template TSV to create which will contain synonym deletions.')
+        help='Path to ROBOT template TSV to create which will contain synonym deletions; exists in Mondo but not '
+             'in source(s) for a given mapping.')
     parser.add_argument(
         '-u', '--outpath-updated', required=True,
-        help='Path to robot template TSV to create which will contain synonym metadata changes.')
+        help='Path to ROBOT template TSV to create which will contain updates to synonym scope predicate; cases where '
+             'the synonym exists in Mondo and on the mapped source term, but the scope predicate is different.')
     sync_synonyms(**vars(parser.parse_args()))
 
 
