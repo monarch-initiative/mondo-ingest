79e63fdf5afbd65d97edcb4c8d660e56b26fcfeb
Joe Flack <joeflack4@gmail.com>
2024-12-17 16:28:00 -0500

    Bug: Synonym Sync: Duplicate rows #684 - Bug fix: Sometimes the multi-value source synonym field would be erroneously set as 'synonym'.

--- src/scripts/sync_synonym.py
+++ src/scripts/sync_synonym.py
@@ -172,9 +172,11 @@ def _common_operations(
     # Format
     if not df_is_combined:
         # - Acronyms: Use source case
-        #   This operation prevents capitalization from being lost, as sometimes Mondo has used lowercase.
-        df['synonym'] = df.apply(lambda row:
-            row['synonym_case_source'] if MONDO_ABBREV_URI in row['synonym_type'] else row['synonym'], axis=1)
+        #   This operation prevents capitalization from being lost, as sometimes Mondo has used lowercase. However,
+        #   ignore cases where 'synonym_case_source' is multi-value.
+        df['synonym'] = df.apply(lambda row: row['synonym_case_source']
+            if MONDO_ABBREV_URI in row['synonym_type'] and not row['synonym_case_mondo_is_many']
+            else row['synonym'], axis=1)
         # - Add ROBOT columns for each synonym scope
         synonym_scopes = ['exact', 'broad', 'narrow', 'related']
         for scope in synonym_scopes:
